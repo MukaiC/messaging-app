41ef18628f990745ca267ebc493c407f9c4bc933
Chiharu Mukai <49642982+MukaiC@users.noreply.github.com>
2020-06-15 10:14:54 +0200

    Created layout.html and modified index.html accordingly

--- application.py
+++ application.py
@@ -1,6 +1,6 @@
 import os
 
-from flask import Flask, render_template
+from flask import Flask, render_template, session
 from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
@@ -8,9 +8,16 @@ app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 socketio = SocketIO(app)
 
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def index():
-    return render_template("index.html")
+    # First time to visit the page
+    if session.get("name") is None:
+        return render_template("index.html")
+
+    return render_template("channels.html")
+
+
+
 
 
 
