0cddb9e0009c1123ffd1c2f241c0ae290d67bd40
Chiharu Mukai <49642982+MukaiC@users.noreply.github.com>
2020-06-15 20:38:50 +0200

    Created channel.js, added socketio

--- application.py
+++ application.py
@@ -1,26 +1,36 @@
 import os
 
-from flask import Flask, render_template, session
+from flask import Flask, render_template, request, session
 from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 socketio = SocketIO(app)
 
+# channels = {}
 
 @app.route("/", methods=["GET", "POST"])
 def index():
     # First time to visit the page
-    if session.get("name") is None:
-        return render_template("index.html")
+    if session.get("name") is not None:
+        return render_template("channels.html")
 
-    return render_template("channels.html")
+    if request.method == "POST":
+        name = request.form.get("name")
+        session["name"] = name
+        return render_template("channels.html")
 
+    return render_template("index.html")
 
+@socketio.on("submit channel")
+def channels(data):
+    channel = data["channel"]
+    # !!! add channel to channels
 
+    emit("announce channel", {"channel": channel}, broadcast=True)
 
 
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    socketio.run(app)
---
+++ static/channels.js
@@ -0,0 +1,22 @@
+document.addEventListener('DOMContentLoaded', () => {
+  // Connect to websocket
+  var socket = io.connect(location.protocol + '//' + document.domain + ':' + location.port);
+
+  // When connected, configure buttons
+  socket.on('connect', () => {
+    // When a new channel is submited
+    document.querySelector('#new-channel').onsubmit = () => {
+      const channel = document.querySelector('#channel').value;
+      socket.emit('submit channel', {'channel': channel});
+      document.querySelector('#channel').value = ''
+      return false;
+    };
+  });
+
+  // When a new channel is announced, add to the unordered list
+  socket.on('announce channel', data => {
+    const li = document.createElement('li');
+    li.innerHTML = `New channel created: ${data.channel}`;
+    document.querySelector('#channels').append(li);
+  });
+});
