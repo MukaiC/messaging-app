392a3a432752341f6b493572450917cb63233be6
Chiharu Mukai <49642982+MukaiC@users.noreply.github.com>
2020-06-16 14:27:22 +0200

    List of current channels is displayed. Users can create a new channel

--- application.py
+++ application.py
@@ -1,32 +1,37 @@
 import os
 
-from flask import Flask, render_template, request, session
+from flask import Flask, render_template, request, session, jsonify
 from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 socketio = SocketIO(app)
 
-# channels = {'test-channel': [{'name': 'domo'}, {'text': 'testing'}]}
+# stored_channels = [{'room':'channel 1', 'messages': [{'name': 'domo', 'text': 'message1'}, {'name': 'domo', 'text': 'message2'}]}, {'room': 'channel 2', 'messages': [{'name':'domo', 'text':'testing2'}, {'name':'kirby', 'text': 'poyo'}]}]
+
+stored_channels = []
 
 @app.route("/", methods=["GET", "POST"])
 def index():
     # First time to visit the page
+    # stored_channels = jsonify(stored_channels)
     if session.get("name") is not None:
-        return render_template("channels.html")
+        return render_template("channels.html", channels=stored_channels)
 
     if request.method == "POST":
         name = request.form.get("name")
         session["name"] = name
-        return render_template("channels.html")
+        return render_template("channels.html", channels=stored_channels)
 
     return render_template("index.html")
+
 
 @socketio.on("submit channel")
 def channels(data):
     channel = data["channel"]
-    # !!! add channel to channels
-
+    # !!! add a new channel to stored_channels
+    new_channel={'room': channel, 'messages': []}
+    stored_channels.append(new_channel)
     emit("announce channel", {"channel": channel}, broadcast=True)
 
 
