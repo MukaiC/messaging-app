ada588ee7476003cb6e255051a504d94742054f0
Chiharu Mukai <49642982+MukaiC@users.noreply.github.com>
2020-06-23 13:59:40 +0200

    Restructured to a single page application

--- application.py
+++ application.py
@@ -1,6 +1,6 @@
 import os
 
-from flask import Flask, render_template, request, session, jsonify
+from flask import Flask, render_template, request,  jsonify  #session
 from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
@@ -17,18 +17,8 @@ socketio = SocketIO(app)
 stored_channels = [{'room':'channel 1', 'messages': [{'name': 'domo', 'text': 'message1'}, {'name': 'domo', 'text': 'message2'}]}]
 # stored_channels = []
 
-@app.route("/", methods=["GET", "POST"])
+@app.route("/")
 def index():
-    # First time to visit the page
-    # stored_channels = jsonify(stored_channels)
-    if session.get("name") is not None:
-        return render_template("channels.html", channels=stored_channels)
-
-    if request.method == "POST":
-        name = request.form.get("name")
-        session["name"] = name
-        return render_template("channels.html", channels=stored_channels)
-
     return render_template("index.html")
 
 @app.route("/messages")
@@ -55,7 +45,7 @@ def channels(data):
 @socketio.on("add message")
 def messages(data):
     name = session["name"]
-    # time = 
+    # time =
     pass
 
 
--- static/index.js
+++ static/index.js
@@ -1,3 +1,5 @@
+
+
 const template = Handlebars.compile(document.querySelector('#new-channel-item').innerHTML);
 document.addEventListener('DOMContentLoaded', () => {
   // Connect to websocket
@@ -17,7 +19,7 @@ document.addEventListener('DOMContentLoaded', () => {
     };
     // When a new channel is submited
     document.querySelector('#new-channel').onsubmit = () => {
-      const channel = document.querySelector('#channel').value;
+      const channel = document.querySelector('#channel').value.trim();
       socket.emit('create channel', {'channel': channel});
       document.querySelector('#channel').value = '';
       return false;
@@ -33,7 +35,7 @@ document.addEventListener('DOMContentLoaded', () => {
     };
     // When a new message is submitted
     document.querySelector('#new-message').onsubmit = () => {
-      const message = document.querySelector('#message').value;
+      const message = document.querySelector('#message').value.trim();
       const time = Date.now()
       socket.emit('add message', {'message': message, 'time':time});
       document.querySelectr('#message').value = '';
@@ -52,4 +54,27 @@ document.addEventListener('DOMContentLoaded', () => {
   socket.on('alert', data => {
     alert(`${data.message}`);
   });
+});
+
+// for a first time user to register a display name
+document.addEventListener('DOMContentLoaded', () => {
+  if (!localStorage.getItem('name')) {
+      document.querySelector('#user').style.display = 'none';
+  } else {
+      document.querySelector('#register').style.display = 'none';
+  };
+  document.querySelector('#submit-name').disabled = true;
+  document.querySelector('#new-name').onkeyup = () => {
+    let newname = document.querySelector('#new-name').value.trim();
+    if (newname.length > 0)
+      document.querySelector('#submit-name').disabled = false;
+    else
+      document.querySelector('#submit-name').disabled = true;
+    };
+  document.querySelector('#register').onsubmit = () => {
+    let new_user = document.querySelector('#new-name').value.trim();
+    localStorage.setItem('name', new_user);
+    alert(`Welcome ${new_user}!`);
+    return false;
+  };
 });
